034563847c6a60583b00af0a657a4bc7b31c6e12
Sveinar Søpler <cybermax@dexter.no>
2024-12-29 18:54:06 +0100

    Add NvOFAPICreateInstanceCuda - stub
    Signed-off-by: Sveinar Søpler <cybermax@dexter.no>

---
+++ src/nvofapi_cuda.cpp
@@ -0,0 +1,45 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include "util/util_env.h"
+#include "util/util_log.h"
+#include "util/util_string.h"
+#include "util/util_statuscode.h"
+#include "../version.h"
+#include "../config.h"
+
+using namespace dxvk;
+extern "C" {
+    NV_OF_STATUS __cdecl NvOFAPICreateInstanceCuda(uint32_t apiVer, void* functionList) {
+        uint32_t apiVerMajor = (apiVer & 0xfffffff0) >> 4;
+        uint32_t apiVerMinor = (apiVer & 0xf);
+        constexpr auto n = __func__;
+
+        if (log::tracing())
+            log::trace(n, apiVer, log::fmt::ptr(functionList));
+
+        log::info(str::format("OFAPI Client Version: ", apiVerMajor, ".", apiVerMinor));
+
+        return ErrorGeneric(n);
+    }
+}
