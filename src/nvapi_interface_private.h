7b69abfe02c758eab52563ef1d1ba657fa4de62e
Sveinar Søpler <cybermax@dexter.no>
2024-12-31 15:17:07 +0100

    Add NvAPI_CUDA_EnumComputeCapableByTopology - Stub
    Somewhat speculative call used in PhysX Snippets from PhysX SDK
    
    Signed-off-by: Sveinar Søpler <cybermax@dexter.no>

--- src/nvapi_interface_private.h
+++ src/nvapi_interface_private.h
@@ -4,4 +4,5 @@ struct NVAPI_INTERFACE_TABLE nvapi_interface_private_table[] = {
     // Non documented function pointers
     { "NvAPI_GPU_GetGPCMask", 0x7dbe90ab },
     { "NvAPI_GPU_GetTPCMaskOnGPC", 0xed74af30 },
+    { "NvAPI_CUDA_EnumComputeCapableByTopology", 0x36e39e6b },
 };
