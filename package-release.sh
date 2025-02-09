b79c76b8ed4f22a6c6302e1a18f25e2739bebf21
Sveinar Søpler <cybermax@dexter.no>
2025-02-09 21:12:48 +0100

    Add optional private nvapi functiontable implementation
    Signed-off-by: Sveinar Søpler <cybermax@dexter.no>

--- package-release.sh
+++ package-release.sh
@@ -48,19 +48,20 @@ while [ $# -gt 0 ]; do
 done
 
 function prepare {
-  python3 validate-methods.py \
-    src/nvapi.cpp             \
-    src/nvapi_sys.cpp         \
-    src/nvapi_disp.cpp        \
-    src/nvapi_mosaic.cpp      \
-    src/nvapi_ngx.cpp         \
-    src/nvapi_drs.cpp         \
-    src/nvapi_gpu.cpp         \
-    src/nvapi_d3d.cpp         \
-    src/nvapi_d3d11.cpp       \
-    src/nvapi_d3d12.cpp       \
-    src/nvapi_vulkan.cpp      \
-    src/nvapi_interface.cpp   \
+  python3 validate-methods.py     \
+    src/nvapi.cpp                 \
+    src/nvapi_sys.cpp             \
+    src/nvapi_disp.cpp            \
+    src/nvapi_mosaic.cpp          \
+    src/nvapi_ngx.cpp             \
+    src/nvapi_drs.cpp             \
+    src/nvapi_gpu.cpp             \
+    src/nvapi_d3d.cpp             \
+    src/nvapi_d3d11.cpp           \
+    src/nvapi_d3d12.cpp           \
+    src/nvapi_vulkan.cpp          \
+    src/nvapi_interface.cpp       \
+    src/nvapi_interface_private.h \
     external/nvapi/nvapi_interface.h
 }
 
