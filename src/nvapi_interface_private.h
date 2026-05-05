17dc1758c41fae301a885b80b670e68bca10f6f5
Sveinar Søpler <cybermax@dexter.no>
2026-05-05 15:42:47 +0200

    Add NvAPI_GPU_GetTPCMaskOnGPC - Stub
    Signed-off-by: Sveinar Søpler <cybermax@dexter.no>

--- src/nvapi_interface_private.h
+++ src/nvapi_interface_private.h
@@ -3,4 +3,5 @@
 struct NVAPI_INTERFACE_TABLE nvapi_interface_private_table[] = {
     // Non documented function pointers
     { "NvAPI_GPU_GetGPCMask", 0x7dbe90ab },
+    { "NvAPI_GPU_GetTPCMaskOnGPC", 0xed74af30 },
 };
